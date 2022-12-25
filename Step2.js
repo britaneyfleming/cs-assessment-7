8b6a9104b6a3b68559ade29c67c03c74460ac6fe
Britaney Fleming <britaneyrfleming@gmail.com>
2022-12-25 10:06:49 -0500

    final commit

---
+++ Step2.js
@@ -0,0 +1,47 @@
+//Sum to zero:
+let value = false;
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        value = true;
+      } 
+    }
+  }
+}
+
+console.log(value)
+
+//Unique characters:
+function hasUniqueCharacters(word) {
+    let uniqueCharacters = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueCharacters.add(word[i])
+    }
+    return uniqueCharacters.size === word.length
+}
+
+//Pangram Sentence:
+function isPangram(string){
+    let strArr = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    for (let i = 0; i < alphabet.length; i++) {
+      if(strArr.indexOf(alphabet[i]) < 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+//Longest Word:
+function findLongestWord(str) {
+    var strSplit = str.split(' ');
+    var longestWord = 0;
+    for(var i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){
+      longestWord = strSplit[i].length;
+       }
+    }
+    return longestWord;
+  }
